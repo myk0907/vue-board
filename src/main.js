015726940c883fef198ec133dc5bfb3ec4d85f0a
mykim <myk0907000@gmail.com>
2023-05-31 17:55:42 +0900

    프로젝트 소개 카드 css 수정

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 import './assets/common.css'
 
-// // 부트스트랩
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
--- src/views/PageAbout.vue
+++ src/views/PageAbout.vue
@@ -5,11 +5,11 @@
     
     <!-- 소개 -->
     <div class="contents">
-      <div class="group_row box1">
+      <div class="group_row box1" style="margin-right: 50px">
         <img alt="Vue logo" src="../assets/logo.png">
       </div>
       <div class="group_row contents box1">
-        div2 ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
+        가나다라마바사아자차카타파하가갸거겨고교구규그기카캬커켜코쿄쿠큐크키
       </div>
     </div>
     <!-- https://www.svgrepo.com/collection/twemoji-emojis/1  svg 이모지 사이트-->
@@ -18,18 +18,18 @@
     <h1 class="header_1"><img class="svg_icon_h1" src="../assets/svg/magnifying-glass.svg"> Header1</h1>
     <h1 class="division_line"></h1>
     <div>
-      <div class="group_row box2">
+      <div class="group_row box-2">
         <div class="contents box1">
           <div class="header_2"><img class="svg_icon_h2" src="../assets/svg/books.svg"> Header1-1</div>
           <div> 
-            div3 ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
+            가나다라마바사아자차카타파하가갸거겨고교구규그기카캬커켜코쿄쿠큐크키
             <li>ddd</li>
             <li>ddd</li>
             <li>ddd</li>
           </div>
         </div>
       </div>
-      <div class="group_row box2">
+      <div class="group_row box-2">
         <div class="contents box1">
           <div class="header_2"><img class="svg_icon_h2" src="../assets/svg/graduation-cap.svg"> Header1-1</div>
           dd
@@ -57,39 +57,37 @@
     <h1 class="header_1"><img class="svg_icon_h1" src="../assets/svg/file-box.svg"> Header3</h1>
     <h1 class="division_line"></h1>
     <div class="card_item">
-      <div><img src="../assets/img/test_img.jpg" style="width: 100%"></div>
+      <div class="card_img_box"><img src="../assets/img/test_img.jpg"></div>
       <div class="card_title">title</div>
       <div class="card_contents">
-        dddddddddddddddddddddddddddddddd<br>
-        dddddddddddddddddddddddddddd
+        안녕하세요.<br>
+        반갑습니다.
+      </div>
+    </div>
+    <div class="card_item" style="cursor: pointer;" onclick="window.open('https://github.com/myk0907/project_shop')">
+      <div class="card_img_box"><img src="../assets/img/test_img.jpg"></div>
+      <div class="card_title">title</div>
+      <div class="card_contents">
+        안녕하세요.<br>
+        반갑습니다.
+      </div>
+    </div>
+    <div class="card_item" style="cursor: pointer;" onclick="window.open(https://github.com/myk0907/final-project_jejuMap')">
+      <div class="card_img_box"><img src="../assets/img/test_img.jpg"></div>
+      <div class="card_title">title</div>
+      <div class="card_contents">
+        안녕하세요.<br>
+        반갑습니다.
       </div>
     </div>
     <div class="card_item">
-      <div><img src="../assets/img/test_img.jpg" style="width: 100%"></div>
+      <div class="card_img_box"><img src="../assets/img/test_img.jpg"></div>
       <div class="card_title">title</div>
       <div class="card_contents">
-        dddddddddddddddddddddddddddddddd<br>
-        dddddddddddddddddddddddddddd
+        안녕하세요.<br>
+        반갑습니다.
       </div>
     </div>
-    <div class="card_item">
-      <div><img src="../assets/img/test_img.jpg" style="width: 100%"></div>
-      <div class="card_title">title</div>
-      <div class="card_contents">
-        dddddddddddddddddddddddddddddddd<br>
-        dddddddddddddddddddddddddddd
-      </div>
-    </div>
-    <div class="card_item">
-      <div><img src="../assets/img/test_img.jpg" style="width: 100%"></div>
-      <div class="card_title">title</div>
-      <div class="card_contents">
-        dddddddddddddddddddddddddddddddd<br>
-        dddddddddddddddddddddddddddd
-      </div>
-    </div>
-      
-      
 
     <!-- contact -->
     <h1 class="header_1"><img class="svg_icon_h1" src="../assets/svg/mailbox.svg"> Header4</h1>
@@ -100,3 +98,15 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    // handleMouseOver(event) {
+    // const targetElement = event.currentTarget;
+    // // targetElement.classList.add('card_hover'); // 클래스 추가
+    // console.log(targetElement);
+    // }
+  }
+}
+</script>
